5425b9eb361de1b794b8edd2575181bb3f0b72d0
ayushresthaaa <ayushrestha2fb5@gmail.com>
2026-04-25 19:29:58 +0545

    created ENUMS defined

---
+++ FleetFactory.Domain/Enums/PartRequestStatus.cs
@@ -0,0 +1,3 @@
+namespace FleetFactory.Domain.Enums;
+
+public enum PartRequestStatus { Pending, Sourced, Rejected }
---
+++ FleetFactory.Domain/Enums/StockMovement.cs
@@ -0,0 +1,3 @@
+namespace FleetFactory.Domain.Enums;
+
+public enum StockMovementType { Purchase, Sale, Adjustment, Return }
